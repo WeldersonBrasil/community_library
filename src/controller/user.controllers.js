7fe766f4d4ab8c0438ddb4b649e65d6ed35a8134
WeldersonBrasil <weldersonbrasil@gmail.com>
2025-07-18 23:29:21 -0300

    feat: dynamic update query

--- src/controller/user.controllers.js
+++ src/controller/user.controllers.js
@@ -1,4 +1,4 @@
-import userService from '../service/user_services.js';
+import userService from '../service/user.services.js';
 
 async function createUserController(req, res) {
     const newUser = req.body;
@@ -11,6 +11,53 @@ async function createUserController(req, res) {
     }
 }
 
-export default {
-    createUserController
+async function findAllUserController(req, res) {
+    try {
+        const user = await userService.findAllUserService();
+        res.send({ user });
+    } catch (e) {
+        return res.status(404).send(e.message);
+    }
 }
+
+async function findUserByIdController(req, res) {
+    const { id } = req.params;
+
+    try {
+        const user = await userService.findUserByIdService(id);
+        res.send({ user });
+    } catch (e) {
+        return res.status(404).send(e.message);
+    }
+}
+
+ async function updateUserController(req, res) {
+    const { id } = req.params;
+    const newUser = req.body;
+
+    try {
+        const user = await userService.updateUserService(newUser, id);
+        res.send({ user });
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+ }
+
+ async function deleteUserController(req, res) {
+    const { id } = req.params;
+
+    try {
+        const message = await userService.deleteUserService(id);
+        res.send({ message });
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+ }
+
+export default {
+    createUserController,
+    findAllUserController,
+    findUserByIdController,
+    updateUserController,
+    deleteUserController
+};
--- src/repositories/user.repositories.js
+++ src/repositories/user.repositories.js
@@ -3,8 +3,8 @@ import db from '../config/database.js'
 db.run(`
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        email TEXT UNIQUE NOT NULL,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
         avatar TEXT
     )
@@ -13,7 +13,7 @@ db.run(`
 function createUserRepository(newUser) {
     return new Promise((resolve, reject) => {
         const {username, email, password, avatar } = newUser;
-        db.run(
+        db.run (
             `
                 INSERT INTO users (username, email, password, avatar)
                 VALUES (?, ?, ?, ?)
@@ -23,7 +23,7 @@ function createUserRepository(newUser) {
                 if (err) {
                     reject(err)
                 } else {
-                    resolve({id: this.lastID, ...newUser})
+                    resolve({ id: this.lastID, ...newUser })
                 }
             }
         );
@@ -50,7 +50,94 @@ function findUserByEmailRepository(email) {
     })
 }
 
+function findUserByIdRepository(id) {
+    return new Promise((resolve, reject) => {
+        db.get(
+            `
+            SELECT id, username, email, avatar
+            FROM users
+            WHERE id = ?
+            `,
+            [id],
+            (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            }
+        );
+    });
+}
+
+ function findAllUserRepository() {
+    return new Promise((resolve, reject) => {
+        db.all(
+            `
+            SELECT id, username, email, avatar FROM users
+            `,
+            [],
+            (err, rows) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve(rows)
+                }
+            }
+        )
+    });
+ }
+
+function updateUserRepository(id, user) {
+    return new Promise((resolve, reject) => {
+        const fields = ["username", "email", "password", "avatar"];
+        let query = "UPDATE users SET";
+        const values = [];
+
+        fields.forEach((field) => {
+            if(user[field] !== undefined) {
+                query += ` ${field} = ?,`;
+                values.push(user[field]);
+            }
+        });
+
+        query = query.slice(0, -1);
+
+        query += " WHERE id =?"
+        values.push(id);
+
+        db.run(query, values, (err) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ ...user, id });
+            }
+        })
+    });
+}
+
+async function deleteUserRepository(id) {
+    return new Promise((resolve, reject) => {
+        db.run(
+            `
+            DELETE FROM users
+            WHERE id = ?
+            `, [id], (err) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve({ message: "User deleted successfully", id })
+                }
+            }
+        );
+    });
+}
+
 export default {
     createUserRepository,
-    findUserByEmailRepository
+    findUserByEmailRepository,
+    findUserByIdRepository,
+    findAllUserRepository,
+    updateUserRepository,
+    deleteUserRepository
 };
